2202985900ea6e1ed5130fd4fef8f2716cf25249
Kyle Reichert <skotekalx@gmail.com>
2026-05-23 17:20:29 -0600

    Add naming project naming feature and refactor.

--- main.cpp
+++ main.cpp
@@ -11,56 +11,48 @@
 std::string sdl3Body(std::string name = "Project") { 
 
   std::string cmakebody = 
-
   std::format(R"(cmake_minimum_required(VERSION 3.30)
 
-  project({})
+project({})
 
-  set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-  set(CMAKE_CXX_STANDARD 23)
-  set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set(CMAKE_CXX_STANDARD 23)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-  include(FetchContent)
+include(FetchContent)
 
-  FetchContent_Declare(
-    SDL3
-    GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-    GIT_TAG release-3.4.4
-  )
+FetchContent_Declare(
+  SDL3
+  GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
+  GIT_TAG release-3.4.4
+)
 
-  FetchContent_MakeAvailable(SDL3)
+FetchContent_MakeAvailable(SDL3)
 
 
-  add_executable(${{PROJECT_NAME}} main.cpp)
+add_executable(${{PROJECT_NAME}} main.cpp)
 
-  Rule of thumb: use private for binaries, public for libraries
-  target_link_libraries(${{PROJECT_NAME}} PRIVATE
-    SDL3::SDL3
-  )
-
-  )", name);
+target_link_libraries(${{PROJECT_NAME}} PRIVATE
+  SDL3::SDL3
+)
+)", name);
   return cmakebody;
 }
 
-std::expected<void, std::string> sdl3Init(std::string name = "Project") {
-  std::string filename = "test.txt";
+std::expected<void, std::string> init(std::function<std::string(std::string)> body, std::string name = "Project") {
+  std::string filename = "test.txt"; // Change this to CMakeList.txt after testing
 
   if (std::filesystem::exists(filename)) {
     return std::unexpected("CMakeList.txt already exists!");
-  } else {
-    std::ofstream file;
-    file.open(filename);
-
-    if (!file.is_open()) {
-      return std::unexpected("File failed to open!");
-    } 
-    else {
-      file << sdl3Body(name);
-    }
-
   }
-  
-  std::cout << "CMakeList.txt created for SDL3 project: " << name << '\n';
+
+  std::ofstream file(filename);
+
+  if (!file.is_open()) {
+    return std::unexpected("File failed to open!");
+  }
+
+  file << body(name);
 
   return {};
 }
@@ -81,30 +73,16 @@ int main(int argc, char* argv[]) {
 
   std::vector<std::string> args(argv, argc + argv);
  
- /*
-  Not sure if this is the best approach. For each primary command, the command 
-  should be an expected type handled byt the handleResult function. Then this 
-  itself is called in a lambda function. The iterator seems to like this format.
- */ 
-
-  std::unordered_map<std::string, std::function<void()>> commands {
-    { "--sdl3", []() { handleResult(sdl3Init()); }},
+  // Valid flags for CMakeList.txt types
+  std::unordered_map<std::string, std::function<void(std::string)>> commands {
+    { "--sdl3", [](std::string name) { handleResult(init(sdl3Body, name)); }},
   };
-  
+
   std::string cmd = args[1];
-   
-  // Find the argument or output error
+  std::string name = (argc > 2) ? argv[2] : "Project";
   if (auto it = commands.find(cmd); it != commands.end()) {
-    it->second();
+    it->second(name);
   } else {
-    std::cerr << "Unkown Command: " << cmd << '\n';
+    std::cerr << "Unknown Command: " << cmd << '\n';
   }
-
-  // for (int i = 1; i < argc; i++) {
-  //   std::cout << argv + i << "\n";
-  // }
-  
-
-
-  // 
 }
