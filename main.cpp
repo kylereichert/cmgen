c9d32d777660e0177637f44a1b71883cd6009bf4
Kyle Reichert <skotekalx@gmail.com>
2026-05-23 15:26:07 -0600

    Implement logic creation for sdl3 CMakeList.txt

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <expected>
+#include <filesystem>
+#include <format>
 #include <iostream>
 #include <unordered_map>
 #include <functional>
@@ -5,48 +8,68 @@
 #include <vector>
 #include <fstream>
 
-std::string sdl3_cmake = R"(
-cmake_minimum_required(VERSION 3.30)
+std::string sdl3Body(std::string name = "Project") { 
 
-project({})
+  std::string cmakebody = 
 
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-set(CMAKE_CXX_STANDARD 23)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
+  std::format(R"(cmake_minimum_required(VERSION 3.30)
 
-include(FetchContent)
+  project({})
 
-FetchContent_Declare(
-  SDL3
-  GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-  GIT_TAG release-3.4.4
-)
+  set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+  set(CMAKE_CXX_STANDARD 23)
+  set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-FetchContent_MakeAvailable(SDL3)
+  include(FetchContent)
+
+  FetchContent_Declare(
+    SDL3
+    GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
+    GIT_TAG release-3.4.4
+  )
+
+  FetchContent_MakeAvailable(SDL3)
 
 
-add_executable(${{PROJECT_NAME}} main.cpp)
+  add_executable(${{PROJECT_NAME}} main.cpp)
 
-Rule of thumb: use private for binaries, public for libraries
-target_link_libraries(${{PROJECT_NAME}} PRIVATE
-  SDL3::SDL3
-)
+  Rule of thumb: use private for binaries, public for libraries
+  target_link_libraries(${{PROJECT_NAME}} PRIVATE
+    SDL3::SDL3
+  )
 
-)";
+  )", name);
+  return cmakebody;
+}
 
-void sdl3Init(std::string name = "default") {
-  std::ofstream file;
-  file.open("text.txt");
-  if (!file.is_open()) {
-    std::cerr << "Error creating file" << '\n';
+std::expected<void, std::string> sdl3Init(std::string name = "Project") {
+  std::string filename = "test.txt";
+
+  if (std::filesystem::exists(filename)) {
+    return std::unexpected("CMakeList.txt already exists!");
   } else {
-    file << sdl3_cmake;
-    file.close();
-    
-    std::cout << "CMakeList.txt created for SDL3 project: " << name << '\n';
-    std::cout << "Note, if you need another version, change the git tag!" << '\n';
+    std::ofstream file;
+    file.open(filename);
+
+    if (!file.is_open()) {
+      return std::unexpected("File failed to open!");
+    } 
+    else {
+      file << sdl3Body(name);
     }
 
+  }
+  
+  std::cout << "CMakeList.txt created for SDL3 project: " << name << '\n';
+
+  return {};
+}
+
+void handleResult(std::expected<void, std::string> result) {
+  // Handles the results for the main CLI arguments
+  if (!result) {
+    std::cerr << "Init Failed: " << result.error() << "\n";
+  }
 }
 
 int main(int argc, char* argv[]) {
@@ -57,25 +80,24 @@ int main(int argc, char* argv[]) {
   }  
 
   std::vector<std::string> args(argv, argc + argv);
-  
-  // Command mappings
+ 
+ /*
+  Not sure if this is the best approach. For each primary command, the command 
+  should be an expected type handled byt the handleResult function. Then this 
+  itself is called in a lambda function. The iterator seems to like this format.
+ */ 
+
   std::unordered_map<std::string, std::function<void()>> commands {
-    { "--sdl3", []() { std::cout << "yeah" << '\n'; }},
-    { "Butt", []() { std::cout << "bah" << '\n'; }},
+    { "--sdl3", []() { handleResult(sdl3Init()); }},
   };
   
   std::string cmd = args[1];
    
-  // Test
-  sdl3Init("yeah");
-
-
   // Find the argument or output error
-  if (auto arg = commands.find(cmd); arg != commands.end()) {
-    arg->second();
+  if (auto it = commands.find(cmd); it != commands.end()) {
+    it->second();
   } else {
     std::cerr << "Unkown Command: " << cmd << '\n';
-    return 1;
   }
 
   // for (int i = 1; i < argc; i++) {
